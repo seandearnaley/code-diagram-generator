47148449c00402114a6f8cc23a5590c101120251
Sean Dearnaley <seandearnaley@hotmail.com>
2023-06-26 04:54:11 -0400

    tweaks, update version

--- code_diagram_generator/main.py
+++ code_diagram_generator/main.py
@@ -1,18 +1,20 @@
 """ Generate a report of the python code outline of a folder. """
-from python_code_outline import python_report_generator
+from folder_tree_generator import generate_tree
+from python_code_outline import get_report
 
 # Define the root folder
 ROOT_FOLDER = "/Users/seandearnaley/Documents/GitHub/reddit-gpt-summarizer"
 
 # Specify the report and ignore file paths (optional)
 REPORT_FILE_PATH = "custom_report.txt"
-IGNORE_FILE_PATH = ROOT_FOLDER + ".gitignore"
+IGNORE_FILE_PATH = ROOT_FOLDER + "/.gitignore"
+FOLDER_TREE = generate_tree(ROOT_FOLDER, ignore_file_path=IGNORE_FILE_PATH)
 
 # Generate the report
-REPORT = python_report_generator.get_report(ROOT_FOLDER)
+REPORT = get_report(ROOT_FOLDER, ignore_file_path=IGNORE_FILE_PATH)
 
 # Write the report to a file
 with open(REPORT_FILE_PATH, "w", encoding="utf-8") as file:
-    file.write(REPORT)
+    file.write(FOLDER_TREE + "\n\n" + REPORT)
 
 print(f"Report generated successfully to {REPORT_FILE_PATH}.")
