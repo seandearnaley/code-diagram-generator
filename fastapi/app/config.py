273722fb2d894ea814725d633f4667f761833b86
Sean Dearnaley <seandearnaley@hotmail.com>
2023-08-01 03:45:08 -0400

    add llm config, move config files

--- fastapi/app/config.py
+++ fastapi/app/config.py
@@ -1,4 +1,5 @@
 """Configuration for the app."""
 from pathlib import Path
 
-DIAGRAM_CONFIG_PATH = Path("app/diagram_config.json")
+DIAGRAM_CONFIG_PATH = Path("app/config/diagram_config.json")
+LLM_CONFIG_PATH = Path("app/config/llm_config.json")
