7191ece561ca4aa545e5133a11b09935dc9d55aa
Sean Dearnaley <SeanDearnaley@hotmail.com>
2023-08-12 20:05:40 -0400

    Update formDefaults.ts

--- nextjs/src/config/formDefaults.ts
+++ nextjs/src/config/formDefaults.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SOURCE_FOLDER = "";
-export const DEFAULT_DIAGRAM_CATEGORY = "";
+export const DEFAULT_DIAGRAM_CATEGORY = "flowchart";
 export const DEFAULT_LLM_VENDOR = "open_ai";
 export const DEFAULT_DIAGRAM_OPTION = "";
 export const DEFAULT_LLM_MODEL = "gpt-3.5-turbo-16k";
