04b59dc0caa757a9ff6d038da0a016b5367ca332
Sean Dearnaley <SeanDearnaley@hotmail.com>
2023-08-21 14:53:54 -0400

    Update MermaidDiagram.tsx

--- nextjs/src/components/MermaidDiagram.tsx
+++ nextjs/src/components/MermaidDiagram.tsx
@@ -7,6 +7,7 @@ import { FC } from "react";
 import SVGRenderer from "@/components/SVGRenderer";
 import getSvgDimensions from "@/lib/getSvgDimensions";
 import { ArrowDownOnSquareIcon, BoltIcon } from "@heroicons/react/24/solid";
+
 import { toPng } from "html-to-image";
 
 type MermaidDiagramProps = {
@@ -15,6 +16,8 @@ type MermaidDiagramProps = {
 };
 
 export const MermaidDiagram: FC<MermaidDiagramProps> = ({ values, text }) => {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const [diagramUrl, setDiagramUrl] = useState("");
   const imageRef = useRef(null);
 
@@ -25,6 +28,8 @@ export const MermaidDiagram: FC<MermaidDiagramProps> = ({ values, text }) => {
 
   const postMermaid = async () => {
     try {
+      setError("");
+      setLoading(true);
       const response = await fetch(
         "http://localhost:8000/mermaid_design_request/",
         {
@@ -54,11 +59,18 @@ export const MermaidDiagram: FC<MermaidDiagramProps> = ({ values, text }) => {
           setImageDimensions(dimensions);
         };
         reader.readAsText(blob);
+        setLoading(false);
       } else {
-        console.error("Failed to create diagram:", response.statusText);
+        const msg = "Failed, response not ok:" + response.statusText;
+        setError(msg);
+        setLoading(false);
+        console.error(msg);
       }
     } catch (error: any) {
-      console.error("Failed to create diagram:", error.message);
+      const msg = `Failed to create diagram: ${error.message}`;
+      setError(msg);
+      setLoading(false);
+      console.error(msg);
     }
   };
 
@@ -75,6 +87,25 @@ export const MermaidDiagram: FC<MermaidDiagramProps> = ({ values, text }) => {
   return (
     <div className="flex justify-center mb-4">
       <div>
+        {error ? <p className="text-red-500 text-sm mt-1">{error}</p> : null}
+        {/* Loading Modal */}
+        {loading && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
+            <div className="p-4 bg-white rounded-md">
+              <p className="mb-2 text-gray-600 flex items-center">
+                Generating design...
+                <span className="ml-2 w-5 h-5 border-2 border-blue-500 rounded-full animate-spin"></span>{" "}
+                {/* Adding the spinning icon */}
+              </p>
+              {/* <div className="relative w-full h-2 bg-gray-200 rounded-md">
+                <div
+                  className="absolute left-0 h-2 bg-blue-500 rounded-md"
+                  style={{ width: `${progress}%` }}
+                />
+              </div> */}
+            </div>
+          </div>
+        )}
         <div className="flex justify-center mb-4">
           <button
             className="text-sm font-semibold leading-6 text-black flex items-center cursor-pointer border-2 border-slate-300 rounded-md p-2 bg-slate-200"
@@ -98,7 +129,6 @@ export const MermaidDiagram: FC<MermaidDiagramProps> = ({ values, text }) => {
                 <button
                   className="text-sm font-semibold leading-6 text-black items-center cursor-pointer border-2 border-slate-300 rounded-md p-2 bg-slate-200"
                   type="button"
-                  onClick={postMermaid}
                 >
                   <div className="flex">
                     <ArrowDownOnSquareIcon className="h-5 w-5 mr-2" />
